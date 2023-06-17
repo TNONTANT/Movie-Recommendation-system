b61884837a88245d2e0619c25c48775dc1d2f24f
TNONTANAT <nontanatto@gmail.com>
2023-06-17 14:33:55 +0100

    update code

---
+++ function.py
@@ -0,0 +1,14 @@
+import re
+import pandas as pd
+import numpy as np
+# feature extraction
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# to clean movie title 
+# (remove special char cuz it's make difficault for searching)
+def clean_title(title):
+    # remove char that not in below reg
+    return re.sub("[^a-zA-Z]", " ", title)
+
+
